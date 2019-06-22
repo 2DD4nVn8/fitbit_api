166d41305144f05e6957b7f71890879fcaa10d82
2DD4nVn8 <52078771+2DD4nVn8@users.noreply.github.com>
2019-06-22 10:25:00 +0900

    Update get_hr.py

--- get_hr.py
+++ get_hr.py
@@ -2,10 +2,10 @@ import fitbit_api
 
 client_id= "222DR2Z" 
 secret = "e7f4ba034ca83d9e0de1b04e1b5c38a5" 
-tokenfile = "fitbit_token_qol1.txt"
+tokenfile = "fitbit_token.txt"
 
-qol1 = fitbit_api.fitbitActivities(CLIENT_ID = client_id ,CLIENT_SECRET = secret, TOKEN_FILE = tokenfile)
-hr = qol1.get_hr()
+hr_data = fitbit_api.fitbitActivities(CLIENT_ID = client_id ,CLIENT_SECRET = secret, TOKEN_FILE = tokenfile)
+hr = hr_data.get_hr()
 print(hr)
 
 
